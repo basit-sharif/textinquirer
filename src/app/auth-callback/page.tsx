66d5a8c314f980f23eb93565f0eade66fbd9ab5f
abdulbasit <ba3568008@gmail.com>
2023-10-13 21:07:47 +0500

    removed some code from auth-callback

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,16 +1,16 @@
 import { useRouter, useSearchParams } from "next/navigation"
-import { trpc } from "../_trpc/client";
+// import { trpc } from "../_trpc/client";
 
 const Page = async () => {
     const router = useRouter();
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
 
-    const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
-        onSuccess: ({ success }) => {
-            router.push(origin ? `/${origin}` : `/dashboard`)
-        }
-    })
+    // const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    //     onSuccess: ({ success }) => {
+    //         router.push(origin ? `/${origin}` : `/dashboard`)
+    //     }
+    // })
 
     return (
         <div>Page</div>
